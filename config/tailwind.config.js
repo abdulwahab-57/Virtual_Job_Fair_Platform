565feb4762973b7741194d2c2e783e6d1d387557
Abdul Wahab <abdulwahab.dev.codes@gmail.com>
2025-02-06 01:29:25 +0500

    Complete Landing Page UI Implementation

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -11,6 +11,8 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        roboto: ['Roboto', 'serif'],
+        condensed: ['Roboto Condensed', 'serif'],
       },
     },
   },
