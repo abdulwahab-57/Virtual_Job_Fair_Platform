bc57add2b78fc51eac3dc9debb8b6bd6ffeaaeb5
Abdul Wahab <abdulwahab.dev.codes@gmail.com>
2025-02-06 13:04:09 +0000

    Removed Unused Model Files

--- app/models/gallery_image.rb
+++
@@ -1,8 +0,0 @@
-class GalleryImage < ApplicationRecord
-  # Associations
-  belongs_to :recruiter_profile
-
-  # Validations
-  validates :recruiter_profile_id, presence: true
-  validates :image_url, presence: true
-end
--- app/models/hiring_team_member.rb
+++
@@ -1,9 +0,0 @@
-class HiringTeamMember < ApplicationRecord
-  # Associations
-  belongs_to :recruiter_profile
-
-  # Validations
-  validates :recruiter_profile_id, presence: true
-  validates :name, presence: true
-  validates :designation, presence: true
-end
