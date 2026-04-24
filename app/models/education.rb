b29661f79883dbf2985dd6da497b1cdc4470dcab
Abdul Wahab <abdulwahab.dev.codes@gmail.com>
2026-04-25 00:09:56 +0500

    feat: add DEGREE_OPTIONS, ransackable_attributes, available_graduation_years to Education

--- app/models/education.rb
+++ app/models/education.rb
@@ -1,15 +1,34 @@
 class Education < ApplicationRecord
-   # Associations
-   belongs_to :student_profile
+  belongs_to :student_profile
 
-   # Validations
-   # validates :student_profile_id, presence: true
-   validates :institution_name, allow_blank: true, length: { maximum: 80 }
-   validates :degree, allow_blank: true, length: { maximum: 50 }
-   validates :graduation_year, allow_blank: true,
-             numericality: {
-               only_integer: true,
-               greater_than: 1900,
-               less_than_or_equal_to: -> { Date.current.year + 10 }
-             }
+  DEGREE_OPTIONS = [
+    "BS (Computer Science)",
+    "BS (Artificial Intelligence)",
+    "BS (Software Engineering)",
+    "BS (Business Analytics)",
+    "BS (Electrical Engineering)",
+    "Bachelor of Business Administration"
+  ].freeze
+
+  validates :institution_name, allow_blank: true, length: { maximum: 80 }
+  validates :degree, allow_blank: true, length: { maximum: 50 }
+  validates :graduation_year, allow_blank: true,
+            numericality: {
+              only_integer: true,
+              greater_than: 1900,
+              less_than_or_equal_to: -> { Date.current.year + 10 }
+            }
+
+  def self.ransackable_attributes(auth_object = nil)
+    %w[degree graduation_year]
+  end
+
+  def self.available_graduation_years
+    where(degree: DEGREE_OPTIONS)
+      .where.not(graduation_year: nil)
+      .distinct
+      .pluck(:graduation_year)
+      .sort
+      .reverse
+  end
 end
